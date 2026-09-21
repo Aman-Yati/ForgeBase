0115cb97fc1f35d576aaa84058618b2b2189cdc1
AmanYati <corporateamanyati@gmail.com>
2026-09-21 19:51:30 +0530

    Remove RestTemplate bean - deployment working

--- backend/src/main/java/com/forgebase/BackendApplication.java
+++ backend/src/main/java/com/forgebase/BackendApplication.java
@@ -2,25 +2,12 @@ package com.forgebase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder
-			.setConnectTimeout(Duration.ofSeconds(5))
-			.setReadTimeout(Duration.ofSeconds(10))
-			.build();
 	}
 
 }
